54df77c3e9d20475eeae6db40e638fc226fd52de
victor <victor.barros@pgmais.com.br>
2019-06-10 11:23:00 -0300

    alteração daemon

--- cp3o/bot_cp3o.sh
+++ cp3o/bot_cp3o.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SERVICE='/integracao/sorocred/email/sorocred_cejud_email.py'
+SERVICE='/integracao/bot_telegram/cp3o/bot_c3po.py'
 if ps ax | grep -v grep | grep -v daemon | grep -v log | grep $SERVICE > /dev/null
 then
    echo "..."
